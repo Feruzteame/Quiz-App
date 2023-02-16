b8a8f7e9f638f956bf8c669e145a474900030642
Feruz <59234162+Feruzteame@users.noreply.github.com>
2023-02-16 20:00:06 +0100

    update css

--- src/feedback.js
+++ src/feedback.js
@@ -1,10 +1,10 @@
 export function success(){
-    const sound1 = document.getElementById("sound1")
-    console.log("success")
-    sound1.play()
+  const sound1 = document.getElementById("sound1")
+  console.log("success")
+  sound1.play()
 }
 export function fail(){
-    const sound3 = document.getElementById("sound3")
-    console.log("fail")
-    sound3.play()
-  }
+  const sound3 = document.getElementById("sound3")
+  console.log("fail")
+  sound3.play()
+}
--- src/index.js
+++ src/index.js
@@ -1,81 +1,81 @@
 import 'animate.css';
+
+import { getSum } from "./score.js";
+import {success} from "./feedback.js";
+import {fail} from "./feedback.js";
+
 export const score = [];
-const holder = document.getElementById("holder")
+const holder = document.getElementById("holder");
 const feedbackHolder = document.getElementById("resultHolder");
-import { getSum } from "./score.js"
-import {success} from "./feedback.js"
-import {fail} from "./feedback.js"
 
-let answerInput = document.createElement("input")
-answerInput.id = "answerInput"
-answerInput.placeholder = "Put Your Answer Here ..."
-let inputHolder = document.createElement("P")
-inputHolder.appendChild(answerInput)
+let answerInput = document.createElement("input");
+answerInput.id = "answerInput";
+answerInput.placeholder = "Put Your Answer Here ...";
+let inputHolder = document.createElement("P");
+inputHolder.appendChild(answerInput);
 
-const feedbackDiv = document.createElement("P")
-feedbackDiv.id = "feedbackDiv"
+const feedbackDiv = document.createElement("P");
+feedbackDiv.id = "feedbackDiv";
 
-let button = document.createElement("button")
-button.innerHTML = "Submit"
-button.id = "submitButton"
-inputHolder.appendChild(button)
+let button = document.createElement("button");
+button.innerHTML = "Submit";
+button.id = "submitButton";
+inputHolder.appendChild(button);
 
-let questionLine = document.createElement("P")
-questionLine.id = "questionLine"
-holder.appendChild(inputHolder)
-holder.appendChild(questionLine)
-feedbackHolder.appendChild(feedbackDiv)
+let questionLine = document.createElement("P");
+questionLine.id = "questionLine";
+holder.appendChild(questionLine);
+holder.appendChild(inputHolder);
+feedbackHolder.appendChild(feedbackDiv);
 
-let i = 0
-let questions = {}
+let i = 0;
+let questions = {};
 
 // put the question in box
 function attachToElement() {
-    questionLine.innerHTML = questions[i].question
+  questionLine.innerHTML = questions[i].question;
 }
 
-
 // fetch data using API
-export let url ;
+export let url;
 export let fetchQuestions = async (url) => {
-   try {
-        const res = await fetch(url);
-        if (!res.ok) {
-            throw new Error(res.status);
-        }
-        const data = await res.json();
-        console.log(data);
-        questions = data.results
-        attachToElement()
-    } catch (error) {
-        console.log(error);
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(res.status);
     }
+    const data = await res.json();
+    console.log(data);
+    questions = data.results;
+    attachToElement();
+    } catch (error) {
+      console.log(error);
+  }
 }
 
 // Choice of Question
 document.addEventListener("DOMContentLoaded", function(e) {
 
-    // Put the button into a variable
-    var e = document.getElementById("myForm");
-    
-    // Wait for user to click the button
-    e.addEventListener( "change", function() {
-    
-      // Put the selected value into a variable
-      var myChoice = this.choice.value;
-      
-      // The "Switch" statement.
-      switch ( myChoice ) {
-      
+  // Put the button into a variable
+  var e = document.getElementById("myForm");
+
+  // Wait for user to click the button
+  e.addEventListener( "change", function() {
+
+    // Put the selected value into a variable
+    var myChoice = this.choice.value;
+
+    // The "Switch" statement.
+    switch ( myChoice ) {
       case "film":
         url = "https://opentdb.com/api.php?amount=5&category=11&difficulty=easy&type=multiple"
         fetchQuestions(url)
         break;
-  
+
       case "music":
         url = "https://opentdb.com/api.php?amount=5&category=12&difficulty=easy&type=multiple"
         fetchQuestions(url)
-  
+
       case "video":
         url = "https://opentdb.com/api.php?amount=5&category=15&difficulty=easy&type=multiple"
         fetchQuestions(url)
@@ -85,71 +85,71 @@ document.addEventListener("DOMContentLoaded", function(e) {
             url = "https://opentdb.com/api.php?amount=5&category=16&difficulty=easy&type=multiple"
             fetchQuestions(url)
             break;
-    case "general":
+      case "general":
         url = "https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple"
         fetchQuestions(url)
         break;
-        }
-    }, false);
-  });
+    }
+  }, false);
+});
 
 // feedback success or fail message
 function responseMsg(is_success, correct_answer) {
-    
-    if (is_success) {
-        const successMsg = document.createElement("P");
-        successMsg.className = "animate__animated animate__slideInRight"
-        successMsg.id = "successMsg"
-        successMsg.id = "successDiv"
-        feedbackDiv.appendChild(successMsg)
-        successMsg.innerHTML = `<img id="img" src="https://img.icons8.com/emoji/48/000000/nikita-clapping-hands-emoji.png"/><span>Good Job!</span>`
-    } else {
-        const failMsg = document.createElement("P");
-        failMsg.className = "animate__animated animate__slideInRight"
-        failMsg.id = "failMsg"
-        failMsg.id = "failDiv"
-        feedbackDiv.appendChild(failMsg)
-       failMsg.innerHTML = `<img id="img" src="https://img.icons8.com/emoji/48/000000/thinking-face.png"/><span>NOT CORRECT!</span> The correct answer is <span>${correct_answer}.</span>`
-    }
+  if (is_success) {
+    const successMsg = document.createElement("P");
+    successMsg.className = "animate__animated animate__slideInRight";
+    successMsg.id = "successMsg";
+    successMsg.id = "successDiv";
+    feedbackDiv.appendChild(successMsg);
+    successMsg.innerHTML = `GOOD JOB!`;
+  } else {
+    const failMsg = document.createElement("P");
+    failMsg.className = "animate__animated animate__slideInRight"
+    failMsg.id = "failMsg";
+    failMsg.id = "failDiv";
+    feedbackDiv.appendChild(failMsg);
+    failMsg.innerHTML = `NOT CORRECT! The correct answer is <span>${correct_answer}.</span>`;
+  }
 }
+
 // check the Answer
 function checkAndNext() {
-    const userInputValue = answerInput.value
-    const userInputValueLowerCase = userInputValue.toLowerCase()
-    console.log(userInputValueLowerCase)
-    const correct_answer = questions[i].correct_answer.toLowerCase()
-    console.log(correct_answer)
-    if( userInputValueLowerCase == correct_answer){
-        score.push(5)
-        getSum()
-        success()
-        responseMsg(true, correct_answer)
-    }else{
-        score.push(0)
-        getSum()
-        fail()
-        responseMsg(false, correct_answer)   
-    }
-    questionLine.innerHTML = ''
-    answerInput.value = ''
+  const userInputValue = answerInput.value;
+  const userInputValueLowerCase = userInputValue.toLowerCase();
+  console.log(userInputValueLowerCase);
+  const correct_answer = questions[i].correct_answer.toLowerCase();
+  console.log(correct_answer);
+  if( userInputValueLowerCase == correct_answer){
+    score.push(5);
+    getSum();
+    success();
+    responseMsg(true, correct_answer);
+  }else{
+    score.push(0);
+    getSum();
+    fail();
+    responseMsg(false, correct_answer); 
+  }
+    questionLine.innerHTML = '';
+    answerInput.value = '';
 }
 button.addEventListener('click', function() {
-    checkAndNext()
-    i++
-    attachToElement()
+  checkAndNext();
+  i++;
+  attachToElement();
 })
 
 // restart the game
-const restart = document.getElementById("restart")
+const restart = document.getElementById("restart");
 function reloadPage(){
-    window.location = document.URL
+  window.location = document.URL;
 }
 restart.addEventListener("click", reloadPage)
 
 window.addEventListener("load", event => {
-    document.getElementById("restart").onclick = function() {
-        location.assign(location.href);        // or, use `location.assign(location)`
-    }
+  document.getElementById("restart").onclick = function() {
+    location.assign(location.href);
+  }
 });
 
 
